45cb0ee89d874b378955f9304f20cd05d89f1145
ehdtjs0612 <inko51366@naver.com>
2024-06-01 01:53:52 +0900

    comment: 엔트리포인트에 주석 추가

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import 'reflect-metadata';
 import express from 'express';
 import { errorHandling } from './middleware/error.middleware';
-import { wrapper } from './util/wrapper.util';
-import { ResponseEntity } from './util/response.util';
 import { accountRouter } from './domain/account/account.router';
 
 const app = express();
 
+// 전역 미들웨어
+app.use(express.json());
+
+// 라우터 등록
 app.use('/account', accountRouter);
 
+// 에러 핸들링 미들웨어
 app.use(errorHandling());
 
 export default app;
