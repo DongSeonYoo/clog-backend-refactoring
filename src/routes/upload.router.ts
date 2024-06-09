3b2b0b4bf5767f8d71577ef92a94d67699985431
ehdtjs0612 <inko51366@naver.com>
2024-06-09 21:10:57 +0900

    feat: 이미지 업로드하는 api추가

--- src/routes/upload.router.ts
+++ src/routes/upload.router.ts
@@ -25,7 +25,47 @@ uploadRouter.post(
       throw new BadRequestException('파일이 존재하지 않습니다');
     }
 
-    console.log(files.map((e) => e.location));
+    return res.send(ResponseEntity.SUCCESS_WITH(files.map((e) => e.location)));
+  }),
+);
+
+/**
+ * POST /upload/club-banner
+ * @Role User
+ * 동아리 배너 이미지 업로드
+ */
+uploadRouter.post(
+  '/club-banner',
+  loginAuthGuard(),
+  s3Uploader({ fieldName: 'clubBanner', maxCount: 1 }),
+  wrapper(async (req, res, next) => {
+    const files = req.files as Express.MulterS3.File[];
+
+    if (!files) {
+      console.log('파일이 존재하지 않음');
+      throw new BadRequestException('파일이 존재하지 않습니다');
+    }
+
+    return res.send(ResponseEntity.SUCCESS_WITH(files.map((e) => e.location)));
+  }),
+);
+
+/**
+ * POST /upload/post
+ * @Role User
+ * 게시글 이미지 업로드
+ */
+uploadRouter.post(
+  '/club-banner',
+  loginAuthGuard(),
+  s3Uploader({ fieldName: 'postImages', maxCount: 10 }),
+  wrapper(async (req, res, next) => {
+    const files = req.files as Express.MulterS3.File[];
+
+    if (!files) {
+      console.log('파일이 존재하지 않음');
+      throw new BadRequestException('파일이 존재하지 않습니다');
+    }
 
     return res.send(ResponseEntity.SUCCESS_WITH(files.map((e) => e.location)));
   }),
